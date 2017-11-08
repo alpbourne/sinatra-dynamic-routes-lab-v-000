dd02474fdfc8fcfbcd46dc7d7b49799384cd8805
alpbourne <alpbourne@gmail.com>
2017-11-08 20:24:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,13 +18,18 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."    
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get "/:operation/:number1/:number2" do
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    @operation = params[:operation]
+
+    case params[:operation]
+    when "add"
+      "#{@num1} + #{@num2}"
+    when "subtract"
+      "#{@num1} - #{@num2} 
   end
 
 end
